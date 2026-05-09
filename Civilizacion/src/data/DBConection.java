420c36644b7f590416f2f349765c8a362f716edf
Pablo Romero Ramos <romeroramospablo86@gmail.com>
2026-05-09 19:09:21 +0200

    update

--- Civilizacion/src/data/DBConection.java
+++ Civilizacion/src/data/DBConection.java
@@ -5,10 +5,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConection {
+	// Cambiar el Servidor, Puerto y Esquema de la BD
 	private String urlDatos = "jdbc:mysql://localhost:3307/civilizations?serverTimezone=UTC";
-	private String user = "super"; 
-	private String password = "1234";
+		
+	private String user = "super"; // Usuario de la BD
+	private String password = "1234"; // Contraseña
+
 	private String driver = "com.mysql.cj.jdbc.Driver";
+	
 	private Connection conn;
 	
 	public DBConection() {
