b34466582d2842647a00e138e6878874f0ed86d9
Pablo Romero Ramos <romeroramospablo86@gmail.com>
2026-05-12 21:26:31 +0200

    update

--- Civilizacion/src/gui/MainWindow.java
+++ Civilizacion/src/gui/MainWindow.java
@@ -1,6 +1,9 @@
 package gui;
 
-import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -9,26 +12,49 @@ import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
 public class MainWindow extends JFrame {
-	private BufferedImage imagen;
+	private BufferedImage imagenIcono, imagenFondo;
 	private MenuPanel menu;
 	
 	public MainWindow() {
 		setTitle("Civilization");
-		setBounds(300, 150, 900, 600);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
+		int anchoVentana = 900;
+		int altoVentana = 600;
+		
+		setSize(anchoVentana, altoVentana);
+		
+		
+		Toolkit pantalla = Toolkit.getDefaultToolkit();
+		Dimension grandaria = pantalla.getScreenSize();
+		
+		int anchoPantalla = grandaria.width;
+		int altoPantalla = grandaria.height;
+	
+		int x = (anchoPantalla - anchoVentana) / 2;
+		int y = (altoPantalla - altoVentana) / 2;
+		setLocation(x, y);
+		
+		
 		try {
-			imagen = ImageIO.read(new File("./src/gui/civilizations_icon.png"));
-			setIconImage(imagen);
+			imagenIcono = ImageIO.read(new File("./src/gui/civilizations_icon.png"));
+			setIconImage(imagenIcono);
+			
+			imagenFondo = ImageIO.read(new File("./src/gui/civilizations_fondo.jpg"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
-		setLayout(new BorderLayout());
-		menu = new MenuPanel();
 		
-		add(menu, BorderLayout.CENTER);
+		menu = new MenuPanel(imagenFondo);
+		add(menu);
 		
+		menu.getBotonExit().addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
 		
 		setVisible(true);
 	}
--- Civilizacion/src/gui/MenuPanel.java
+++ Civilizacion/src/gui/MenuPanel.java
@@ -1,6 +1,11 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -8,19 +13,39 @@ import javax.swing.JPanel;
 public class MenuPanel extends JPanel {
 	private JButton botonPlay, botonExit;
 	private JPanel menuBotones;
+	private BufferedImage fondo;
 	
-	public MenuPanel() {
+	public MenuPanel(BufferedImage imagen) {
+		this.fondo = imagen;
+		
 		setLayout(new BorderLayout());
 		
-		menuBotones = new JPanel();
+		menuBotones = new JPanel(new FlowLayout(FlowLayout.CENTER, 20, 220)) {
+			protected void paintComponent(Graphics g) {
+				
+			}
+		};
 		
 		botonPlay = new JButton("JUGAR");
+		botonPlay.setPreferredSize(new Dimension(130, 45));
+		
 		botonExit = new JButton("SALIR");
+		botonExit.setPreferredSize(new Dimension(130, 45));
 		
 		menuBotones.add(botonPlay);
 		menuBotones.add(botonExit);
 		
-		add(menuBotones, BorderLayout.CENTER);
+		add(menuBotones, BorderLayout.SOUTH);
 	}
 	
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2d = (Graphics2D) g;
+		
+		g2d.drawImage(fondo, 0, 0, getWidth(), getHeight(), this);
+	}
+
+	public JButton getBotonExit() {
+		return botonExit;
+	}
 }
