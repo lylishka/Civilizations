900c9ffc818dd8353d52d14245a2b942f11a4494
Pablo Romero Ramos <romeroramospablo86@gmail.com>
2026-05-12 19:25:28 +0200

    update

--- Civilizacion/src/gui/MenuPanel.java
+++ Civilizacion/src/gui/MenuPanel.java
@@ -1,7 +1,6 @@
 package gui;
 
 import java.awt.BorderLayout;
-import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -10,12 +9,13 @@ public class MenuPanel extends JPanel {
 	private JButton botonPlay, botonExit;
 	private JPanel menuBotones;
 	
-	public MenuPanel() {				
+	public MenuPanel() {
+		setLayout(new BorderLayout());
+		
+		menuBotones = new JPanel();
+		
 		botonPlay = new JButton("JUGAR");
 		botonExit = new JButton("SALIR");
-		
-		botonPlay.setBounds(300, 350, 200, 50);
-		botonExit.setBounds(300, 420, 200, 50);
 		
 		menuBotones.add(botonPlay);
 		menuBotones.add(botonExit);
